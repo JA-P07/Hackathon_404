a0807604eab468f592e189402fbc5ff312fa5263
JA-P07 <julius.palttala@edu.tampere.fi>
2024-05-31 10:16:04 +0300

    pari juttuu

--- script.js
+++ script.js
@@ -19,3 +19,36 @@ document.addEventListener('DOMContentLoaded', (event) => {
         goHomeButton.style.top = `${y}px`;
     });
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const goHomeButton = document.getElementById('go-home3');
+
+    goHomeButton.addEventListener('mouseover', (event) => {
+        const x = Math.random() * (window.innerWidth - goHomeButton.clientWidth);
+        const y = Math.random() * (window.innerHeight - goHomeButton.clientHeight);
+        goHomeButton.style.left = `${x}px`;
+        goHomeButton.style.top = `${y}px`;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const goHomeButton = document.getElementById('go-home4');
+
+    goHomeButton.addEventListener('mouseover', (event) => {
+        const x = Math.random() * (window.innerWidth - goHomeButton.clientWidth);
+        const y = Math.random() * (window.innerHeight - goHomeButton.clientHeight);
+        goHomeButton.style.left = `${x}px`;
+        goHomeButton.style.top = `${y}px`;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const goHomeButton = document.getElementById('go-home_base');
+
+    goHomeButton.addEventListener('mouseover', (event) => {
+        const x = Math.random() * (window.innerWidth - goHomeButton.clientWidth);
+        const y = Math.random() * (window.innerHeight - goHomeButton.clientHeight);
+        goHomeButton.style.left = `${x}px`;
+        goHomeButton.style.top = `${y}px`;
+    });
+});
